3bbc02b9d51217dc04acb723bec198b6c88cb0a1
CorneliaMo <moweihao2@163.com>
2024-09-12 19:41:59 +0800

    Init

---
+++ AutoDNS.cpp
@@ -0,0 +1,31 @@
+//百度公网api
+//https://qifu-api.baidubce.com/ip/local/geo/v1/district
+//返回值
+/*
+{
+    "code": "Success",
+    "data": {
+        "continent": "亚洲",
+        "country": "中国",
+        "zipcode": "518051",
+        "timezone": "UTC+8",
+        "accuracy": "区县",
+        "owner": "中国电信",
+        "isp": "中国电信",
+        "source": "数据挖掘",
+        "areacode": "CN",
+        "adcode": "440305",
+        "asnumber": "4134",
+        "lat": "22.556244",
+        "lng": "113.939291",
+        "radius": "25.3318",
+        "prov": "广东省",
+        "city": "深圳市",
+        "district": "南山区"
+    },
+    "charge": true,
+    "msg": "查询成功",
+    "ip": "218.17.207.60",  
+    "coordsys": "WGS84"
+}
+*/
